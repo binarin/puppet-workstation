3a6759573a4e0d166d0e1c9e1e60533e270383ab
Alexey Lebedeff <binarin@gmail.com>
2014-06-02 22:31:19 +0400

    Emacs source build deps

--- manifests/profile/emacs.pp
+++ manifests/profile/emacs.pp
@@ -2,4 +2,12 @@ class binarin::profile::emacs {
   include ::binarin::profile::fonts
   $packages = [ "emacs24", "emacs24-el" ]
   package { $packages: ensure => latest }
+
+  # for building emacs from sources
+  ensure_packages(["libxpm-dev", "libjpeg8-dev", "libgif-dev", "libtiff4-dev", "libncurses5-dev", "librsvg2-dev", "libmagick++-dev",
+                   "libgpm-dev", "libdbus-1-dev", "libgnutls-dev", "libxml2-dev", "libotf-dev", "ruby-dev"])
+
+  ensure_packages(["fpm"], {provider => gem, require => Package["ruby-dev"]})
+
+
 }
